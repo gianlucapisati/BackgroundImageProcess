27bf97b80ac18928772d9cd5a7d41f65af015e47
Janky <gianluca.pisati@gmail.com>
2014-02-25 14:37:42 +0100

    params

--- www/ios/BackgroundImageProcessPlugin.js
+++ www/ios/BackgroundImageProcessPlugin.js
@@ -1,11 +1,10 @@
 var backgroundimageprocess = {
-	uploadPhotos: function(success, fail){
-		cordova.exec(success, fail, "BackgroundImageProcess", "uploadPhotos",[]);
+	uploadPhotos: function(success, fail, params){
+		cordova.exec(success, fail, "BackgroundImageProcess", "uploadPhotos",params);
 	},
-    downloadPhotos: function(success, fail){
-        cordova.exec(success, fail, "BackgroundImageProcess", "downloadPhotos",[]);
+    downloadPhotos: function(success, fail, params){
+        cordova.exec(success, fail, "BackgroundImageProcess", "downloadPhotos",params);
     }
-
 }
 
 module.exports = backgroundimageprocess;
